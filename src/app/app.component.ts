b9e67d27324d7a561800891f8f17cf7f7c851037
frame88 <fleuzzi97@gmail.com>
2022-08-29 17:51:59 +0200

    template inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>ciao a tutti</h1>',
+  styles: [` h1{ background-color: red;} `]
 })
 export class AppComponent {
   title = 'recap';
