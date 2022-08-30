a36a79bae98cc7ae3797f6f3726784dcd0ce7047
frame88 <fleuzzi97@gmail.com>
2022-08-30 12:00:10 +0200

    scomparsa di un img al click di un <button>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <button (click)="toggle()">Clicca</button>
-    <h1 *ngIf="visible">Hello world</h1>
+    <button (click)="load()">load</button>
+    <button (click)="unload()">unload</button>
+    <img *ngIf="url" [src]="url" alt="">
 
-    <li 
-      [hidden] = !visible
-      *ngFor="let user of users">
-      {{user}}
-    </li>
   `,
 })
 
 export class AppComponent {
   title = 'recap';
-  
-  visible = false;
 
-  toggle() {
-    this.visible = !this.visible;
+  url : string;
+
+  load() {
+    this.url = 'https://www.collinsdictionary.com/images/full/computer_49399603.jpg';
   }
 
-  users = ['mario','gianni','piero'];
-
+  unload() {
+    this.url = null;
+  }
+  
 }
 
 // styles: [` h1{ background-color: red;} `]
