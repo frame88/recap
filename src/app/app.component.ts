067c2d4aa221a04e94eb1c79dcde788224a97655
frame88 <fleuzzi97@gmail.com>
2022-08-30 10:17:58 +0200

    interpolazione

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>ciao a tutti</h1>',
-  styles: [` h1{ background-color: red;} `]
+  template: `
+    <h1>{{esempio}}</h1>
+    <span>{{ esempio === 'stringa esempio' ? 'titolo' : 'altra stringa sconosciuta'}}</span>
+    <h3>{{1+3*5}}</h3>
+  `,
 })
 export class AppComponent {
   title = 'recap';
+
+  esempio = 'stringaesempio'
 }
+
+
+// styles: [` h1{ background-color: red;} `]
